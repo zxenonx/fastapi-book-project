53dcab6057796ee5d1b3356b9945f82f1def9967
zxenonx <xenon.nemrod@gmail.com>
2024-06-02 23:22:08 +0100

    Add update and delete book db functions

--- api/db/schemas.py
+++ api/db/schemas.py
@@ -3,6 +3,8 @@ from typing import OrderedDict
 from pydantic import BaseModel
 
 class Genre(str, Enum):
+    """Book genres."""
+
     SCI_FI = "Science Fiction"
     FANTASY = "Fantasy"
     HORROR = "Horror"
@@ -26,36 +28,55 @@ class InMemoryDB:
     def __init__(self):
         self.books: OrderedDict[int, Book] = {}
 
-    def get_books(self) -> list[Book]:
+    def get_books(self) -> OrderedDict[int, Book]:
+        """Gets books from database.
+
+        Returns:
+            OrderedDict[int, Book]: Ordered dictionary of books.
+        """
         return self.books
     
     def add_book(self, book: Book) -> Book:
+        """Adds book to database.
+
+        Args:
+            book (Book): Book to add.
+
+        Returns:
+            Book: Added book.
+        """
         self.books.update({book.id: book})
 
     def get_book(self, book_id: int) -> Book:
-        return self.books.get(book_id)
-            
+        """Gets a specific book from database.
 
-    books = [
-        {
-            "id": 1,
-            "title": "The Hobbit",
-            "author": "J.R.R. Tolkien",
-            "publication_year": 1937,
-            "genre": Genre.SCI_FI
-        },
-        {
-            "id": 2,
-            "title": "The Lord of the Rings",
-            "author": "J.R.R. Tolkien",
-            "publication_year": 1954,
-            "genre": Genre.FANTASY
-        },
-        {
-            "id": 3,
-            "title": "The Return of the King",
-            "author": "J.R.R. Tolkien",
-            "publication_year": 1955,
-            "genre": Genre.FANTASY
-        }
-    ]
+        Args:
+            book_id (int): Book ID.
+
+        Returns:
+            Book: Book.
+        """
+        return self.books.get(book_id)
+    
+    def update_book(self, book_id: int, data: Book) -> Book:
+        """Updates a specific book in database.
+
+        Args:
+            book_id (int): Book ID.
+            data (Book): Book data.
+
+        Returns:
+            Book: Updated book.
+        """
+        self.books.update({book_id: data})
+        return self.books.get(book_id)
+
+    def delete_book(self, book_id: int) -> None: 
+        """Deletes a specific book from database.
+
+        Args:
+            book_id (int): Book ID.
+        """
+        if book_id in self.books:
+            del self.books[book_id]
+            
